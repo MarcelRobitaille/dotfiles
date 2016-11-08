f99df3778e0c364da74dd87e17d20afd129e21e4
Marcel Robitaille <marcelrobitaille11@gmail.com>
2016-11-08 14:56:22 -0400

    Added python file that parses wttr.in for conky

---
+++ bin/parseWeather.py
@@ -0,0 +1,25 @@
+#!/bin/python
+import requests
+import time
+import re
+
+def internet():
+      try:
+        requests.get('http://216.58.219.206', timeout=1)
+        return True
+      except Exception as ex:
+        return False
+
+while not internet():
+    time.sleep(1)
+
+ansi_escape = re.compile(r'\x1b[^m]*m')
+
+r = requests.get('http://wttr.in', headers = {'User-Agent': 'curl'})
+lines = r.text.split('\n')
+lines = lines[2:7]
+lines = '\n'.join(lines)
+lines = ansi_escape.sub('', lines)
+lines = lines.split('\n')
+for line in lines:
+    print("{} $alignr {}".format(line[:15], line[15:].strip()))
