0c4b5057273dfedfd38731a12fb848d49b06a1f2
Marcel Robitaille <marcelrobitaille11@gmail.com>
2016-11-18 11:38:10 -0400

    Added most recent update time to weather. Not yet sure if I like this

--- bin/parseWeather.py
+++ bin/parseWeather.py
@@ -15,6 +15,12 @@ while not internet():
 
 ansi_escape = re.compile(r'\x1b[^m]*m')
 
+timestamp = time.strftime('%l:%M %p')
+
+print('''${{alignc}}Weather
+${{voffset -20}}${{alignr}}${{color grey}}{}
+${{voffset -8}}$hr${{voffset 10}}'''.format(timestamp))
+
 r = requests.get('http://wttr.in', headers = {'User-Agent': 'curl'})
 lines = r.text.split('\n')
 lines = lines[2:7]
