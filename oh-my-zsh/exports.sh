d1aabd94065a213f270f5b34411cd121199ff3a8
Marcel Robitaille <marcelrobitaille11@gmail.com>
2016-11-18 11:38:47 -0400

    Fixed some shit. I am dumb

--- oh-my-zsh/exports.sh
+++ oh-my-zsh/exports.sh
@@ -1,8 +1,8 @@
 export PATH="/home/marcel/.config/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH"
 export FPATH="/home/marcel/.oh-my-zsh/custom/plugins:$FPATH"
 
-export EDITOR="/usr/bin/atom"
-export EDITOR_CLI="/usr/sbin/vim"
+export EDITOR="/usr/sbin/vim"
+export VISUAL="/usr/bin/atom"
 export CHEATCOLORS=true
 
 export LANG=en_US.UTF-8
