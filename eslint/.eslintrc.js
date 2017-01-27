32737319db9ed1171b2ec1b88566710b48148390
Marcel Robitaille <marcelrobitaille11@gmail.com>
2017-01-27 11:02:17 -0400

    Switched from jshint to eslint

---
+++ eslint/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true,
+    'commonjs': true
+  },
+
+  'extends': 'eslint:recommended',
+
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+
+  'plugins': [ 'react' ],
+
+  'rules': {
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [ 'error', 'unix' ],
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'never' ],
+    'react/jsx-uses-vars': 1,
+    'no-console': 0,
+  }
+}
