7da9c04c1a3ce3190139f54488d562e21a024581
Marcel Robitaille <marcelrobitaille11@gmail.com>
2017-06-21 16:18:51 -0300

    Fix battery notification issues like DISPLAY

--- cron/battery.sh
+++ cron/battery.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+source ~/.oh-my-zsh/exports.sh
+
 LVL=$(cat /sys/class/power_supply/BAT1/capacity)
 STATUS=$(cat /sys/class/power_supply/BAT1/status)
 REMAINING="$(acpi | awk '{ print $5 }')"
 
-if [[ "$LVL" -lt 200 ]] && [[ "$STATUS" == "Discharging" ]]; then
-  notify -w 450 --urgent "Low battery!. $REMAINING remaining."
+if [[ "$LVL" -lt 20 ]] && [[ "$STATUS" == "Discharging" ]]; then
+  DISPLAY=:0 notify -w 450 --urgent "Low battery!. $REMAINING remaining."
 fi
 
