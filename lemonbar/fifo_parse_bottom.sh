49801fd3c42add665e1184fa877642fa51d9167d
Marcel Robitaille <marcelrobitaille11@gmail.com>
2017-08-04 16:08:56 -0300

    Desktops in panel are now per monitor

--- lemonbar/fifo_parse_bottom.sh
+++ lemonbar/fifo_parse_bottom.sh
@@ -3,10 +3,6 @@
 while read -r line; do
   source "$HOME/.cache/wal/colors.sh"
   case $line in
-    W*)
-      WORKSPACES="$(zsh scripts/workspace.sh)"
-      TITLE="$(zsh scripts/title.sh)"
-      ;;
     C*)
       CPU="${line#?}"
       ;;
@@ -17,6 +13,17 @@ while read -r line; do
       NETWORK="%{U$color1}%{+u} ${line#?} %{-u}"
       ;;
   esac
-  BAR="%{l}%{B$color0 F$color5} $WORKSPACES%{c}$TITLE%{r}$NETWORK %{U$color2}%{+u}%{A:xfce4-taskmanager:} $CPU %{-u} %{U$color3}%{+u} $RAM %{A}%{-u}%{U-}"
-  echo "$BAR%{S+}$BAR"
+
+  function BAR {
+    WORKSPACES=$(zsh scripts/workspace.sh $MONITOR)
+    TITLE="$(zsh scripts/title.sh)"
+    echo -n "%{l}%{B$color0 F$color5} $WORKSPACES%{c}$TITLE%{r}$NETWORK %{U$color2}%{+u}%{A:xfce4-taskmanager:} $CPU %{-u} %{U$color3}%{+u} $RAM %{A}%{-u}%{U-}"
+  }
+
+  result=""
+  bspc query -M | while read -r MONITOR; do
+    result+="$(BAR)%{S+}"
+  done
+  echo "$result"
 done
+
--- lemonbar/scripts/workspace.sh
+++ lemonbar/scripts/workspace.sh
@@ -1,18 +1,37 @@
-input=$(wmctrl -d)
-
-workspace() {
-  echo "%{A:wmctrl -s $1:} %{A}"
+workspace_populated() {
+  echo -n "%{A:wmctrl -s $1:} %{A}"
 }
 
-workspace_empty="%{A:wmctrl -s \1:} %{A}"
-workspace_current=" "
+workspace_empty() {
+echo -n "%{A:wmctrl -s $1:} %{A}"
+}
 
-workspaces=$(wmctrl -d | awk '{ print $2$1 }' | tr -d '[\n]')
+workspace_current() {
+  echo -n " "
+}
 
-wmctrl -l | awk '{ print $2 }' |
+local -a workspaces
+workspaces=($(bspc query -m "$1" -D --names))
 
-while read -r window; do
-  workspaces=${workspaces/-$window/$(workspace $window)}
+local -a populated
+populated=($(wmctrl -l | awk '{ print $2 }'))
+
+for (( i=1; i<=$#workspaces; i++ )); do
+  workspace=$workspaces[i]
+
+  # Check if it's the current desktop
+  if [[ $workspace == $(bspc query -D -d --names) ]]; then
+    workspace_current
+    continue
+  fi
+
+  # Check if populated
+  if [[ ${populated[(r)$workspace]} == $workspace ]]; then
+    workspace_populated $workspace
+    continue
+  fi
+
+  # Empty and not selected
+  workspace_empty $workspace
 done
 
-echo "$workspaces" | sed -e "s/\*[0-9]/$workspace_current/g" -e "s/\-\([0-9]\)/$workspace_empty/g"
