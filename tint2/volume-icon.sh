a02bb283961e979d6d7139fccf9b86fffdc4c35a
Marcel Robitaille <mail@marcelrobitaille.me>
2019-10-13 20:03:22 -0300

    Make panel volume indicator listen to events
    Rather than polling periodically, listen to events from `pactl
    subscribe`.

--- tint2/volume-icon.sh
+++ tint2/volume-icon.sh
@@ -2,16 +2,28 @@
 
 num_icons=4
 
-if [[ $(pamixer --get-mute) == "true" ]]; then
-  level='mute'
-  icon='mute'
-else
-  level=$(pamixer --get-volume)
+# Line output from `pactl subscribe` when volume actually changes
+change_match="Event 'change' on sink"
 
-  ((icon = level * num_icons / 101))
-  level="$level%"
-fi
+# Kickstart loop by echoing magic string before subscribing
+(echo "$change_match*"; pactl subscribe) | while read line; do
 
-echo "$HOME/.config/tint2/icons/volume_$icon.svg"
-echo "$level"
+	# Prevent feedback loop of
+	# Reading volume creates events in `pactl subscribe`
+	# Without this check, processing events creates events creating a feedback
+	# loop
+	[ "$line" = "$change_match" ] || continue
 
+	if [[ $(pamixer --get-mute) == "true" ]]; then
+		level='mute'
+		icon='mute'
+	else
+		level=$(pamixer --get-volume)
+
+		((icon = level * num_icons / 101))
+		level="$level%"
+	fi
+
+	echo "$HOME/.config/tint2/icons/volume_$icon.svg"
+	echo "$level"
+done
