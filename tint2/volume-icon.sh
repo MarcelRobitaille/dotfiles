d5e7e01755f92d2a165ce071915015c46b2d4acf
Marcel Robitaille <mail@marcelrobitaille.me>
2019-10-22 13:29:35 -0300

    Filter events with grep rather than condition in loop

--- tint2/volume-icon.sh
+++ tint2/volume-icon.sh
@@ -6,24 +6,20 @@ num_icons=4
 change_match="Event 'change' on sink"
 
 # Kickstart loop by echoing magic string before subscribing
-(echo "$change_match*"; pactl subscribe) | while read line; do
+(echo "$change_match"; pactl subscribe) |
+	grep --line-buffered "$change_match" |
+	while read line; do
 
-	# Prevent feedback loop of
-	# Reading volume creates events in `pactl subscribe`
-	# Without this check, processing events creates events creating a feedback
-	# loop
-	[ "$line" = "$change_match" ] || continue
+		if [[ $(pamixer --get-mute) == "true" ]]; then
+			level='mute'
+			icon='mute'
+		else
+			level=$(pamixer --get-volume)
 
-	if [[ $(pamixer --get-mute) == "true" ]]; then
-		level='mute'
-		icon='mute'
-	else
-		level=$(pamixer --get-volume)
+			((icon = level * num_icons / 101))
+			level="$level%"
+		fi
 
-		((icon = level * num_icons / 101))
-		level="$level%"
-	fi
-
-	echo "$HOME/.config/tint2/icons/volume_$icon.svg"
-	echo "$level"
-done
+		echo "$HOME/.config/tint2/icons/volume_$icon.svg"
+		echo "$level"
+	done
