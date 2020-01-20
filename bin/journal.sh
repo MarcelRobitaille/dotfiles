afd4aa1e8543f9aba80bf4b615044815ab52c3ee
Marcel Robitaille <mail@marcelrobitaille.me>
2020-01-19 22:36:42 -0400

    Add option to edit yesterday's journal

--- bin/journal.sh
+++ bin/journal.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-filename="$HOME/Nextcloud/journal/$(date "+%-Y.%m.%d").md"
+timestamp="$(date)"
+
+OPTS=`getopt --options y --long yesterday --name 'so' -- "$@"`
+eval set -- "$OPTS"
+
+while true; do
+	case "$1" in
+		-y | --yesterday ) timestamp="$(date -d "1 day ago")"; shift ;;
+		-- ) shift; break ;;
+		* ) break ;;
+	esac
+done
+
+echo $timestamp
+
+shift $(($OPTIND - 1))
+
+filename="$HOME/Nextcloud/journal/$(date -d "$timestamp" "+%-Y.%m.%d").md"
 
 mkdir -p $(dirname $filename)
 
 [ -f "$filename" ] || cat > "$filename" <<EOF
-# $(date "+%B %-d, %Y")
+# $(date -d "$timestamp" "+%B %-d, %Y")
 EOF
 
 $EDITOR $filename
