d86d7a0763d2fbad67eed422551fec8a86e41e33
Marcel Robitaille <mail@marcelrobitaille.me>
2020-06-13 12:07:27 -0300

    Remove unused aliases

--- zsh/custom/aliases.sh
+++ zsh/custom/aliases.sh
@@ -1,11 +1,10 @@
-alias term="(sleep .5; wmctrl -r :ACTIVE: -b toggle,maximized_horz,maximized_vert) & alacritty"
-
 # Basic functions
+alias :q=exit
+alias mux=tmuxinator
 alias clip="xsel -i -b"
 alias incognito="fc -p; SPACESHIP_CHAR_PREFIX=' '"
 
 # Places
-alias dt="cd ~/Desktop"
 alias ll="ls -lah"
 
 # Git
@@ -19,15 +18,3 @@ alias cam="git commit -am "
 alias d="git diff "
 alias p="git push"
 alias pl="git pull"
-alias new="clone git@github.com:Marcel-Robitaille/template.git "
-
-# Misc
-alias t=tmuxinator
-alias starwars="telnet towel.blinkenlights.nl"
-alias fonts="fc-cache -fv"
-alias spaces="sed 's/^/    /'"
-alias :q=exit
-
-# Platformio
-alias sm="pio serialports monitor"
-
