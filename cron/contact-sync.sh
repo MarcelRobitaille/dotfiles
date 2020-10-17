8690fdc3b68a052481ff3666e19521d7b8bacde7
Marcel Robitaille <mail@marcelrobitaille.me>
2020-10-17 13:49:07 -0400

    [cron] Add script to sync phone's contacts
    I use this to get their profile pictures for notifications

---
+++ cron/contact-sync.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CONTACT_DIR="$HOME/.contacts/Contacts"
+
+vdirsyncer discover
+vdirsyncer sync
+
+for f in ${CONTACT_DIR}/*.vcf; do
+	echo
+	echo $f
+	DATA=$(cat "$f" |tr -d "\r\n"|sed -e 's/.*TYPE=//' -e 's/REV:.*//' -e 's/END:VCARD.*//')
+	NAME=$(grep -a '^N\:;' $f|sed -e 's/.*://' -e 's/[;\r]//g')
+
+	#if [ $(wc -c <<< $DATA) -lt 5 ];then #bashism
+	if [ $(echo $DATA|wc -c) -lt 5 ];then
+		echo "No images found in $f"
+		exit 2
+	fi
+	EXT=${DATA%%:*}
+
+	[ "$EXT" == 'BEGIN' ] && continue
+
+	IMG=${DATA#*:}
+	IMG=${IMG// /}
+
+	[ -z "$NAME" ] && continue
+	[ -z "${IMG}" ] && continue
+
+	if ! echo $IMG | base64 -d > "/tmp/${NAME}.${EXT}"; then
+		echo "Failed to decode ${NAME}.${EXT}"
+		continue
+	fi
+
+	convert "/tmp/${NAME}.${EXT}" "${CONTACT_DIR}/${NAME}.png"
+
+	rm "/tmp/${NAME}.${EXT}"
+done
