5fa5dbb63849f85f90fb30df94ca6a4b21c4d1b2
Marcel Robitaille <mail@marcelrobitaille.me>
2020-10-20 21:37:10 -0400

    [zsh] Add hash directories for courses

--- zsh/custom/directories.sh
+++ zsh/custom/directories.sh
@@ -1,1 +1,3 @@
 hash -d weekly="/home/marcel/Nextcloud/Waterloo/QuIN Lab/Weekly report"
+hash -d ece730="/home/marcel/Nextcloud/Waterloo/Courses/ECE730: Introduction to Noise Process"
+hash -d ece606="/home/marcel/Nextcloud/Waterloo/Courses/ECE606: Algorithm Design and Analysis"
