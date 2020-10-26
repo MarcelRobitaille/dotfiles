25171ac18da6a155953180a0c9bf27c5927ff455
Marcel Robitaille <mail@marcelrobitaille.me>
2020-10-26 10:28:33 -0400

    [zsh] Add QuIN hash directory

--- zsh/custom/directories.sh
+++ zsh/custom/directories.sh
@@ -1,3 +1,4 @@
 hash -d weekly="/home/marcel/Nextcloud/Waterloo/QuIN Lab/Weekly report"
 hash -d ece730="/home/marcel/Nextcloud/Waterloo/Courses/ECE730: Introduction to Noise Process"
 hash -d ece606="/home/marcel/Nextcloud/Waterloo/Courses/ECE606: Algorithm Design and Analysis"
+hash -d quin="/home/marcel/Nextcloud/Waterloo/QuIN Lab"
