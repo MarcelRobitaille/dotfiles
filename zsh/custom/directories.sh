a2a864ddb737a8cd9d29e7d2a4309f84cae0d65d
Marcel Robitaille <mail@marcelrobitaille.me>
2021-04-12 09:57:30 -0400

    [zsh] Remove spaces from paths

--- zsh/custom/directories.sh
+++ zsh/custom/directories.sh
@@ -1,5 +1,5 @@
 hash -d dots="/home/marcel/.dotfiles"
-hash -d weekly="/home/marcel/Nextcloud/Waterloo/QuIN Lab/Weekly report"
+hash -d weekly="/home/marcel/Nextcloud/Waterloo/QuIN_Lab/Weekly report"
 hash -d ece657="/home/marcel/Nextcloud/Waterloo/Courses/2021A/ECE657A_Data_and_Knowledge_Modeling_and_Analysis"
-hash -d quin="/home/marcel/Nextcloud/Waterloo/QuIN Lab"
+hash -d quin="/home/marcel/Nextcloud/Waterloo/QuIN_Lab"
 hash -d cmos="/home/marcel/code/quin/CMOS_RTN"
